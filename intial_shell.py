451c7a19331b861e197f0d05d712d5cebfc6c2e9
dfshapir <das8396@g.harvard.edu>
2021-03-19 23:56:10 +0200

    Trying commit again

--- intial_shell.py
+++ intial_shell.py
@@ -1,4 +1,16 @@
 # Adding changes
 print("Hello World!")
-import numpy 
-data1 = numpy.array([1, 2, 3])
+import numpy as ny
+data1 = ny.array([1, 2, 3])
+import matplotlib as mat
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Testing out creation of dataframes.
+
+dict = {
+        "country":["Brazil", "Russia", "India", "China", "South Africa"],
+        "capital":["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"]}
+
+brics = pd.DataFrame(dict)
+brics
